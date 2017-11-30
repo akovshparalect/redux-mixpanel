fdcb1f8d4a58073d09d0e98eb13948483af22d09
Anastasiya Kovsh <a.kovsh@paralect.com>
2017-11-30 16:07:11 +0300

    Update index.js
    Changed a functionality to work with 'react-native-mixpanel'

--- src/index.js
+++ src/index.js
@@ -9,8 +9,49 @@ export default function mixpanelMiddleware(mixpanel) {
     }
 
     try {
-      const { event, props } = action.meta.mixpanel
-      mixpanel.track(event, props)
+      const { event, type, props } = action.meta.mixpanel
+      switch (type) {
+            case 'track': {
+              mixpanel.track(event);
+              break;
+            }
+            case 'trackWithProperties': {
+              mixpanel.trackWithProperties(event, props);
+              break;
+            }
+            case 'set': {
+              mixpanel.set(props);
+              break;
+            }
+            case 'setOnce': {
+              mixpanel.setOnce(props);
+              break;
+            }
+            case 'timeEvent': {
+              mixpanel.timeEvent(event);
+              break;
+            }
+            case 'registerSuperProperties': {
+              mixpanel.registerSuperProperties(props);
+              break;
+            }
+            case 'registerSuperPropertiesOnce': {
+              mixpanel.registerSuperPropertiesOnce(props);
+              break;
+            }
+            case 'trackCharge': {
+              mixpanel.trackCharge(event, props);
+              break;
+            }
+            case 'trackChargeWithProperties': {
+              mixpanel.trackChargeWithProperties(event, props);
+              break;
+            }
+            case 'increment': {
+              mixpanel.increment(event, props);
+              break;
+            }
+          }
     }
     catch (error) {
     }
